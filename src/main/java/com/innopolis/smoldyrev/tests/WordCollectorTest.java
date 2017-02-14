2db5bf45996190509b20ac1bd35e1528c13edf43
Egor Smoldyrev <esmoldyrev@gmail.com>
2017-02-14 20:30:47 +0300

    Доб: тесты/логирование

---
+++ src/main/java/com/innopolis/smoldyrev/tests/WordCollectorTest.java
@@ -0,0 +1,43 @@
+package com.innopolis.smoldyrev.tests;
+
+import com.innopolis.smoldyrev.collector.WordCollector;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by smoldyrev on 13.02.17.
+ */
+class WordCollectorTest {
+
+    private static WordCollector wordCollector;
+
+    @BeforeAll
+    public static void init() {
+
+        wordCollector = new WordCollector();
+    }
+
+    @Test
+    void put() {
+
+        assertNotNull(wordCollector);
+
+        wordCollector.put("тест1");
+        wordCollector.put("тест2");
+        wordCollector.put("тест3");
+        wordCollector.put("тест1");
+        wordCollector.put("тест1");
+        wordCollector.put("тест4");
+
+        /*сразу тестим getWordQty*/
+        assertTrue(wordCollector.getWordQty("тест1") == 3);
+        assertTrue(wordCollector.getWordQty("тест2") == 1);
+        assertTrue(wordCollector.getWordQty("тест3") == 1);
+        assertTrue(wordCollector.getWordQty("тест4") == 1);
+
+    }
+
+
+}
